41825ed9c8d16f7ef96b8564169845766c1e8571
wgy <wgy@dst08553.cn1.global.ctrip.com>
2019-09-05 18:55:27 +0800

    add google validation-html

--- core/server/web/site/app.js
+++ core/server/web/site/app.js
@@ -138,6 +138,7 @@ module.exports = function setupSiteApp(options = {}) {
     // Serve robots.txt if not found in theme
     siteApp.use(shared.middlewares.servePublicFile('robots.txt', 'text/plain', constants.ONE_HOUR_S));
     siteApp.use(shared.middlewares.servePublicFile('bdunion.txt', 'text/plain', constants.ONE_HOUR_S));
+    siteApp.use(shared.middlewares.servePublicFile('googlebff2433b10bb75f5.html', 'text/plain', constants.ONE_HOUR_S));
 
     // setup middleware for internal apps
     // @TODO: refactor this to be a proper app middleware hook for internal & external apps
